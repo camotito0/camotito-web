1f969d58664000c225b7f0e22c400b250547a656
camotito0 <cecilianallerena@gmail.com>
2023-02-08 13:19:26 -0500

    Adding icons to social component

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,7 +1,20 @@
+import { 
+  ImLinkedin,
+  ImGithub,
+  ImBehance,
+  ImTwitter 
+} from "react-icons/im";
 
 const Socials = () => {
   return (
-    <div className="hidden xl:flex ml-24">Socials</div>
+    <div className="hidden xl:flex ml-24">
+      <ul>
+        <li><a href="#" target='_blank'><ImLinkedin/></a></li>
+        <li><a href="#" target='_blank'><ImGithub/></a></li>
+        <li><a href="#" target='_blank'><ImBehance/></a></li>
+        <li><a href="#" target='_blank'><ImTwitter/></a></li>
+      </ul>
+    </div>
   )
 };
 

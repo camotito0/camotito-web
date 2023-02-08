021cb78d7c2805b1c4fd756a9f435e46272e18a3
camotito0 <cecilianallerena@gmail.com>
2023-02-08 13:13:21 -0500

    Change style in header component, adding styles, jsx in component mobileNav

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,18 +10,18 @@ const Header = () => {
     lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center'>
       <div className='flex flex-col lg:flex-row lg:items-center w-full justify-between'>
 
-          <Link to={'/'}> <img src={Logo} alt='logo img'/></Link>
+          <Link to={'/'} className='max-w-[200px]' > <img src={Logo} alt='logo img'/></Link>
 
-          <nav className='hidden lg:flex gap-x-12 font-semibold'>
+          <nav className='hidden xl:flex gap-x-12 font-semibold'>
             <Link to={'/'} className='text-[#696c6d] hover:text-primary transition' >Home</Link>
             <Link to={'/about'} className='text-[#696c6d] hover:text-primary transition' >About</Link>
             <Link to={'/portfolio'} className='text-[#696c6d] hover:text-primary transition' >Portfolio</Link>
             <Link to={'/contact'} className='text-[#696c6d] hover:text-primary transition' >Contact</Link>
           </nav>
 
+      </div>
           <Socials/>
           <MobileNav/>
-      </div>
     </header>
   );
 };
--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,15 +1,63 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
-import { IoMdClose } from 'react-icons/io'
-import { CgMenuRight } from 'react-icons/cg'
+import { IoMdClose } from 'react-icons/io';
+import { CgMenuRight } from 'react-icons/cg';
+
+const menuVariants = {
+  hidden: {
+    x: '100%'
+  },
+  show: {
+    x: 0,
+    trasition: {
+      ease: [0.6, 0.01, -0.05, 0.5]
+    }
+  }
+}
 
 const MobileNav = () => {
+  const [ openMenu, setOpenMenu ] = useState(false);
+
   return (
-    <nav>
-      <div className='text-3xl cursor-pointer'>
+    <nav className='text-primary xl:hidden'>
+      {/* menu open button */}
+      <div
+        onClick={() => setOpenMenu(true)}
+        className='text-3xl cursor-pointer'
+      >
         <CgMenuRight/>
       </div>
+
+      {/* menu */}
+      <motion.div 
+        variants={menuVariants}
+        initial='hidden'
+        animate={openMenu ? 'show' : '' }
+        className='bg-white shadow-2xl 
+          w-full absolute top-0 right-0 
+          max-w-xs h-screen z-20'
+      >
+          {/* icons */}
+          <div 
+            onClick={() => setOpenMenu(false)}
+            className='text-4xl absolute z-30 
+              left-4 top-14 text-primary cursor-pointer'
+          >
+            <IoMdClose/>
+          </div>
+
+          {/* menu list */}
+          <ul className='h-full flex flex-col 
+              justify-center items-center gap-y-8
+              text-primary font-primary 
+              font-bold text-3xl'>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/portfolio'>Portfolio</Link></li>
+            <li><Link to='/contact'>Contact</Link></li>
+          </ul>
+      </motion.div>
     </nav>
   );
 };
